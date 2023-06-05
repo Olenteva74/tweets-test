7e33131bc17876e87e6e97a469931cdae169eae8
Olenteva74 <olenteva74@ukr.net>
2023-06-05 14:24:52 +0300

    Clean memory

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,16 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Welcome } from 'components/Welcome';
+import { cleanMemory } from 'store/userSlice';
 
 const Home = () => {
-    return <Welcome/>
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(cleanMemory());
+  }, [dispatch]);
+
+    return <Welcome/>;
 }
 
 export default Home;
